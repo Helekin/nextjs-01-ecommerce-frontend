185cdd4259df0d7227a124ed6adcc46fe6081210
Helekin <clsjs94@hotmail.com>
2023-05-09 06:45:46 -0500

    home

---
+++ src/pages/home/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { BasicLayout } from "@/layouts";
+
+export default function home() {
+  return (
+    <BasicLayout>
+      <h2>Home</h2>
+    </BasicLayout>
+  );
+}
